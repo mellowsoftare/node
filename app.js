564531f30511480af9366e1d1857ee2bcc7dfe45
Brandon McCarthy <brandon@Kacies-MacBook.local>
2020-08-02 20:30:36 -0500

    update port

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
   res.send("Welcome to the home page!");
 });
 
-app.listen(3000, () => {
+app.listen(port () => {
   console.log("yoooo");
 });
