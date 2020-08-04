8557d417dc492bd346cceab051d9a1c39b0de49f
Brandon McCarthy <brandon@Kacies-MacBook.local>
2020-08-03 20:50:28 -0500

    chat app

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 //v1.0.1
 const express = require('express');
+
+const socket = require('socket.io');
+const port = process.env.port || 3000;
+
+//App Setup
 const app = express();
 
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send("Welcome to the home page!");
-});
+var io = socket(server);
 
-const port = process.env.port || 3000;
-app.listen(port, () => {
-  console.log("yoooo");
+io.on('connection', (socket) => {
+  console.log('made socket connection', socket.id);
+
+  // Handle chat event
+  socket.on('chat', (data) => {
+    io.sockets.emit('chat', data);
+  });
+
+  // Handle Typing
+  socket.on('typing', (data) => {
+      socket.broadcast.emit('typing', data);
+      console.log(data);
+  });
+
+});//End On Connection
+
+var server = app.listen(port, () => {
+  console.log('listening to requests on port 3000');
 });
